e87db48fb6559373ab8a32c6f3a6aaede4f3dc96
unko-chan <kevinliang0303@gmail.com>
2021-01-07 09:50:44 +0000

    implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] != [${expected}]`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  for (let genre in object) {
+    if (object[genre] === value) {
+      return genre
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  gamers: "Minecraft the Movie"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Minecraft the Movie"), "gamers");
