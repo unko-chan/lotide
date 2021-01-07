a89d1c415b6fc4b317222a5bbcc75f2228012337
unko-chan <kevinliang0303@gmail.com>
2021-01-07 04:15:40 +0000

    D3 updates

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(
-      `🛑🛑🛑 Assertion Passed: [${actual}] === [${expected}]`
+      `✅✅✅ Assertion Passed: [${actual}] === [${expected}]`
     );
   } else if (actual !== expected) {
     console.log(
-      `✅✅✅ Assertion Failed: [${actual}] != [${expected}]`
+      `🛑🛑🛑 Assertion Failed: [${actual}] != [${expected}]`
     );
   }
 };
---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] != [${expected}]`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+      } else {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
