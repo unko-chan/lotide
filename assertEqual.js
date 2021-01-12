17e3e72d675438807f464162de9fbf56f799d6c6
unko-chan <kevinliang0303@gmail.com>
2021-01-12 14:24:10 -0800

    refactoring

--- assertEqual.js
+++ assertEqual.js
@@ -4,7 +4,6 @@ const assertEqual = function (actual, expected) {
   } else if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] != [${expected}]`);
   }
-};
+}; 
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
