077675425bd7b2d8a2773adbb0091f041151824d
unko-chan <kevinliang0303@gmail.com>
2021-01-13 17:02:41 -0800

    refactoring lotide

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] != [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (arr1, arr2) {
   if (arr1.length === arr2.length) {
@@ -21,3 +15,5 @@ const eqArrays = function (arr1, arr2) {
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = eqArrays;
--- middle.js
+++ middle.js
@@ -1,24 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-      } else {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function (array) {
   const output = [];
